24515a928f069de6dc62f55a9cccf108a803faba
Joe <80287580+joevs360@users.noreply.github.com>
2024-01-31 17:50:18 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function gerarInserts(){
    document.getElementById("sql").value =  "";
-   var textos = document.getElementById("json").value.replaceAll('"{','{').replaceAll('},','}').replaceAll('}"','}').replaceAll('\n','').split("{");
+   var textos = document.getElementById("json").value.replaceAll('"{','{').replaceAll('},','}').replaceAll('}"','}').replaceAll('\n','').replaceAll("NULL","null").split("{");
    var tabela = document.getElementById("tabela").value.replaceAll(' ', '');
    var inserts = "";
    for (var i = 0; i < textos.length; i++){
